ad55c9339b4f42602f2e09cc184cd385b149aea6
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-03-26 15:14:17 -0500

    Packaging: Use Click HOTH fork: click → click-hotoffthehamster.

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='click-alias',
-    version='1.0.1',
-    description='Enable aliases for Click',
+    name='click-hotoffthehamster-alias',
+    # Latest click-contrib/master: f7e4dd40 2019-08-24 v1.0.1.
+    # - (lb): "Upstream" uses 'v' prefix in version tags, but not
+    #   me, so can tag 1.0.1.
+    version='1.0.1a1',
+    description='Add aliases to click-hotoffthehamster commands',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Hot Off The Hamster',
     author_email='hotoffthehamster@gmail.com',
-    url='https://github.com/hotoffthehamster/click-alias',
+    url='https://github.com/hotoffthehamster/click-hotoffthehamster-alias',
     license='MIT',
-    packages=['click_alias'],
+    packages=['click_hotoffthehamster_alias'],
     install_requires=[
-        'click',
+        'click-hotoffthehamster >= 7.1.1 , < 8',
     ],
     extras_require={
         'dev': [
