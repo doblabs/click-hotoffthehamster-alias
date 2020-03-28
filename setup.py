4afc1a6f4bd844135a5c062160a85ccf3f339dbf
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-03-28 17:02:31 -0500

    Release: Bump version (1.0.2).

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='click-hotoffthehamster-alias',
-    # Latest click-contrib/master: f7e4dd40 2019-08-24 v1.0.1.
-    # - (lb): "Upstream" uses 'v' prefix in version tags, but not
-    #   me, so can tag 1.0.1.
-    version='1.0.1a1',
+    version='1.0.2',
     description='Add aliases to click-hotoffthehamster commands',
     long_description=long_description,
     long_description_content_type='text/markdown',
